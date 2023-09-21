eb0030c3df8a3ea6ab1a0e3007f4127326d6398a
Akhil Kumar <akhilguretia@gmail.com>
2023-09-21 05:58:08 +0530

    Build watchedMovies list

--- src/App.js
+++ src/App.js
@@ -24,6 +24,29 @@ const tempMovieData = [
   },
 ];
 
+const tempWatchedData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    runtime: 148,
+    imdbRating: 8.8,
+    userRating: 10,
+  },
+  {
+    imdbID: "tt0088763",
+    Title: "Back to the Future",
+    Year: "1985",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    runtime: 116,
+    imdbRating: 8.5,
+    userRating: 9,
+  },
+];
+
 const App = () => {
   return (
     <>
@@ -60,6 +83,7 @@ const Navbar = () => {
 }
 
 const Main = () => {
+
   return (
     <main className="main">
       <div className="box">
@@ -77,34 +101,18 @@ const Main = () => {
       </div>
 
       <div className="box">
-
         <button className="btn-toggle">+</button>
 
-        <div className="summary">
-          <h2>Movies you watched</h2>
-          <div>
-            <p>
-              <span>#️⃣</span>
-              <span>movies</span>
-            </p>
-            <p>
-              <span>⭐️</span>
-              <span>Imdb</span>
-            </p>
-            <p>
-              <span>🌟</span>
-              <span>User</span>
-            </p>
-            <p>
-              <span>⏳</span>
-              <span>avgRun</span>
-            </p>
-          </div>
-        </div>
+        <Summary />
 
-
+        <ul className="list">
+          {tempWatchedData.map((movie) => (
+            <WatchedMovies movie={movie} />
+          ))}
+        </ul>
       </div>
-    </main>
+
+    </main >
   );
 }
 
@@ -123,6 +131,60 @@ const Movie = ({ movie }) => {
       </div>
 
     </li>
+  );
+}
+
+const Summary = () => {
+
+  return (
+    <div className="summary">
+      <h2>Movies you watched</h2>
+      <div>
+        <p>
+          <span>#️⃣</span>
+          <span>movies</span>
+        </p>
+        <p>
+          <span>⭐️</span>
+          <span>Imdb</span>
+        </p>
+        <p>
+          <span>🌟</span>
+          <span>User</span>
+        </p>
+        <p>
+          <span>⏳</span>
+          <span>avgRun</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+const WatchedMovies = ({ movie }) => {
+
+  return (
+    <li key={movie.imdbID}>
+
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <h3>{movie.Title}</h3>
+
+      <div>
+        <p>
+          <span>⭐️</span>
+          <span>{movie.imdbRating}</span>
+        </p>
+        <p>
+          <span>🌟</span>
+          <span>{movie.userRating}</span>
+        </p>
+        <p>
+          <span>⏳</span>
+          <span>{movie.runtime} min</span>
+        </p>
+      </div>
+
+    </li >
   );
 }
 
